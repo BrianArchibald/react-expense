a1b7fe91233dbfe7d1f4f6a6a8cccbbabaaffe52
Brian Archibald <brianallenarchibald@gmail.com>
2020-05-27 20:14:08 -0500

    added transaction component, passed as prop

--- src/Components/TransactionList.js
+++ src/Components/TransactionList.js
@@ -1,19 +1,17 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
+import { Transaction } from './Transaction';
 
 export const TransactionList = () => {
     const { transactions } = useContext(GlobalContext); //grabbing global state to use here
 
+// bringing in transactions from GlobalContext, then mapping over them to display as a list
     return (
-        <>
-            <h3>History</h3>
-      <ul className="list">
-        { transactions.map(transaction => (
-          <li className="minus">
-          {transaction.text} <span>-$400</span><button className="delete-btn">x</button>
-        </li>
-        ))} 
-      </ul>
-        </>
+      <>
+        <h3>History</h3>
+        <ul className="list">
+          { transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))} 
+        </ul>
+      </>
     )
 }
